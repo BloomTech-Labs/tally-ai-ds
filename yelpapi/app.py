0de35d7f937c4c9ce67ea9448e39a0fa68c297aa
blakelobato <blakelobato1@gmail.com>
2020-06-09 21:27:02 -0700

    flask api barebones

---
+++ yelpapi/app.py
@@ -0,0 +1,16 @@
+""" The main application and routing logic for Yelp Flask API"""
+
+from decouple import config
+from flask import Flask
+from yelpapi.routes.home_routes import home_routes
+
+def create_app():
+    """Application Factory Pattern"""
+    """Create, Configure, and Instance of Flask App"""
+    app = Flask(__name__)
+    CORS(app) #Flask CORS, https://flask-cors.readthedocs.io/en/latest/
+
+    # Registering routes
+    app.register_blueprint(home_routes)
+
+    return app
