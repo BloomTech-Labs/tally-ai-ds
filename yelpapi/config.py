8f7880b9da5a42753d9efbedc7e77a8802de57ed
Harsh Desai <hurshd0@gmail.com>
2020-06-13 01:22:40 +0000

    hursh: chore: re-factoring flask app again to make it contanierized

--- yelpapi/config.py
+++ yelpapi/config.py
@@ -1,16 +1,19 @@
 import os
 import sys
 from decouple import config
-from utils import postgres_helper
+from yelpapi.utils import postgres_helper
 
-class config
-"""Base config for Staging API"""
-db_conn = create_connection()
+class Config:
+    """Base config for Staging API"""
+    #db_conn = create_connection()
 
-class ProductionConfig(Config):
+class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     TESTING = True
+
+class ProductionConfig(Config):
+    pass
 
 def get_config():
     """Set default config to ProductionConfig unless Staging environment is set to false on Linux 'export STAGING=False' or Windows Powershell '$Env:STAGING="False"'. Using os.environ directly will throw errors if not set. For pytest, please use ProductionConfig"""
@@ -18,7 +21,7 @@ def get_config():
         STAGING = os.getenv("STAGING")
     else:
         STAGING = "False"
-    
+
     if STAGING == "True":
         return DevelopmentConfig()
     return ProductionConfig
